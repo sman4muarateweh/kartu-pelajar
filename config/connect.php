e360d9b8c4d4ae0a8cec11098e54ccecafcea53e
4dev <94159177+smafourmute@users.noreply.github.com>
2022-07-20 20:29:27 +0700

    Add files via upload

---
+++ config/connect.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$dbname = "// ========================= nama database ================================";
+$username = "// ========================= username database ================================";
+$password = "// ========================= password database ================================";
+try
+    {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+error_reporting(E_NOTICE ^ E_ALL);
+if (is_file('home.php')){
+    $path = "";
+}elseif (is_file('../home.php')){
+    $path =  "../";
+}elseif (is_file('../../home.php')){
+    $path =  "../../";
+}
+include_once $path."langs/set_lang.php";
+$verifyUser = "<span style='color: #03A9F4;' data-toggle='tooltip' data-placement='top' title='".lang('verified_page')."' class='fa fa-check-circle verifyUser'></span>";
+$usrSessID = $_SESSION['id'];
+$usrRemovedAcc = $conn->prepare("SELECT id FROM signup WHERE id=:usrSessID");
+$usrRemovedAcc->bindParam(':usrSessID',$usrSessID,PDO::PARAM_INT);
+$usrRemovedAcc->execute();
+$$usrRemovedAccCount = $usrRemovedAcc->rowCount();
+if (isset($usrSessID)) {
+	if($$usrRemovedAccCount < 1){
+		session_destroy();
+	}
+}
+?>
