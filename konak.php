92c323269f0450610c84f06e59bdf0fb213045fb
SMAN 4 Muara Teweh <94159177+sman4muarateweh@users.noreply.github.com>
2025-01-16 02:02:51 +0700

    Create konak.php

---
+++ konak.php
@@ -0,0 +1,7 @@
+<?php
+require_once('db');
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
