9302f326ae2ba8d8898cf9545928f50e968d128d
SMAN 4 Muara Teweh <94159177+sman4muarateweh@users.noreply.github.com>
2025-01-16 02:03:43 +0700

    Update konak.php

--- konak.php
+++ konak.php
@@ -1,7 +1,11 @@
 <?php
-require_once('db');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die('Connection failed: ' . $conn->connect_error);
 }
 ?>
